43ceb519baa5c3730aa117c00aac4ff5f61b6ca9
ErebosXYZ <robertmaynou@gmail.com>
2025-04-22 11:00:01 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Buena suerte
+const inputPounds = document.querySelector("#input");
+const weightResult = document.querySelector("#result");
+
+inputPounds.addEventListener("input", (event) =>{
+    const pounds = event.target.value;
+
+    weightResult.textContent = (pounds * 0.453592).toFixed(2);
+})
